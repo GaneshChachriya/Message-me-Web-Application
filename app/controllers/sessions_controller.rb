12d280108802646598f043a0fba9dc7c379ab3d8
ganesh chachriya <g.chachriya11@gmail.com>
2022-03-05 09:46:31 +0000

    add message form ui

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    before_action :logged_in_redirect, only: [:new, :create]
     def new
     end
     def create
@@ -16,5 +17,12 @@ class SessionsController < ApplicationController
         session[:user_id] = nil
         flash[:success] = "you have successfully log out"
         redirect_to login_path
+    end
+    private
+    def logged_in_redirect
+        if logged_in?
+            flash[:error] = "you already logged in "
+            redirect_to root_path
+        end
     end
 end
