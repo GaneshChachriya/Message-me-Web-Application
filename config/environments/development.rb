713bc30fcc6d223413bf8efc7f43b1deb3b736fe
ganesh chachriya <g.chachriya11@gmail.com>
2022-03-07 12:35:27 +0000

    add styling to chatbox

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_cable.allowed_request_origins = ['https://77dabf488f5e4f20ae7fcbacf2caa3f8.vfs.cloud9.ap-south-1.amazonaws.com']
 end
